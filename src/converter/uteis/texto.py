a1c2d7af03dc6e10dcc701e85ffcc6c9a88cd22f
Matheus Bosi <matheusbosir@gmail.com>
2026-01-23 16:11:54 -0300

    fix: Ajusta função que pega texto final linha ate espaço duplo

--- src/converter/uteis/texto.py
+++ src/converter/uteis/texto.py
@@ -1,19 +1,19 @@
 import re
-from typing import Final, Pattern
-
+from re import Pattern
+from typing import Final
 
 REGEX_ULTIMA_COLUNA: Final[Pattern[str]] = re.compile(r'\s{2,}([^\s].*)$')
+
+
 def retorna_texto_final_linha_ate_espaco_duplo(
     linha: str, limpar_linha: bool = True
 ) -> tuple[str, str]:
-    linha = linha.rsplit()
-
     if match := REGEX_ULTIMA_COLUNA.search(linha):
         resultado = match.group(1).strip()
 
         if limpar_linha:
-            linha = linha[:match.start()].strip()
+            linha = linha[: match.start()].strip()
 
         return resultado, linha
 
-    return "", linha
+    return '', linha
